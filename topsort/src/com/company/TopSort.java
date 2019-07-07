0853457a332eb332028d64cbcc567d1848ac0937
DmLvkvch <31544742+DmLvkvch@users.noreply.github.com>
2019-07-07 23:57:44 +0300

    Add files via upload

---
+++ topsort/src/com/company/TopSort.java
@@ -0,0 +1,104 @@
+package com.company;
+
+import java.util.LinkedList;
+import java.util.Stack;
+
+public class TopSort {
+    private Graph graph;
+    private int V;
+    private int E;
+    private boolean[] used;
+    private Stack<Integer> stack;
+    public LinkedList<Integer> ans = new LinkedList<>();
+    public TopSort(Graph g){
+        graph = g;
+        init();
+    }
+
+    void init(){
+        V = graph.V();
+        E = graph.E();
+        used = new boolean[graph.VertexList().size()+1];
+        stack = new Stack<>();
+        for(int i = 0;i<graph.VertexList().size()+1;i++)
+            used[i] = false;
+    }
+/*
+    private void DFS(int pos){
+        used[pos] = true;
+        System.out.println(pos);
+        for(int i = 0;i<graph.checkV(pos).way.size();i++){
+            if(!used[graph.checkV(pos).way.get(i)])
+                DFS(graph.checkV(pos).way.get(i));
+        }
+        stack.push(pos);
+    }
+    void alg() {
+        init();
+        stack.clear();
+        ans.clear();
+        for(int i = 0;i<graph.VertexList().size();i++){
+            if(!used[graph.VertexList().get(i)]) {
+                DFS(graph.VertexList().get(i));
+            }
+        }
+        int k = stack.size();
+        for(int i = 0;i<k;i++) {
+            ans.add(stack.peek());
+            System.out.println(stack.peek());
+            stack.pop();
+        }
+    }
+
+ */
+
+
+    boolean alg(){
+        boolean Cycle = false;
+        for(int i = 0;i<graph.V();i++){
+            Cycle = DFS(graph.VertexList().get(i));
+            if(Cycle) {
+                return false;
+            }
+        }
+        int k = stack.size();
+        for(int i = 0;i<k;i++){
+            ans.add(stack.peek());
+            stack.pop();
+        }
+        return true;
+    }
+
+    boolean DFS(int v){
+        if(graph.checkV(v).c == 1) {
+            System.out.println("CYCLE: "+v);
+            return true;
+        }
+        if(graph.checkV(v).c == 2) {
+            return false;
+        }
+        graph.checkV(v).c = 1;
+        System.out.println("красим "+v+" в серый цвет");
+        for(int i = 0;i<graph.checkV(v).way.size();i++){
+            if(DFS(graph.checkV(v).way.get(i))) {
+                return true;
+            }
+        }
+        stack.push(v);
+        graph.checkV(v).c = 2;
+        System.out.println("красим "+v+" в черный цвет");
+        return false;
+    }
+
+   // boolean dfs(int v){ }
+
+    void alg(Graph g){
+        graph = g;
+        ans.clear();
+        stack.clear();
+        for(int i = 0;i<graph.V();i++){
+            graph.checkV(graph.VertexList().get(i)).c = 0;
+        }
+        alg();
+    }
+}
---
+++ topsort/src/com/company/Window.java
@@ -0,0 +1,54 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Window extends JPanel{
+    Graph graph;
+    TopSort sort;
+    public SourceGraphField graphField;
+    public SortedGraphField sortedGraphField;
+    ///////////////////////////////
+
+    public Window(Graph graph, TopSort sort) {
+        this.sort = sort;
+        this.graph = graph;
+        graphField = new SourceGraphField(graph);
+        //new SourceGraphField(graph);
+        JPanel contentPanel = new JPanel();
+        contentPanel.setLayout(new GridBagLayout());
+
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        constraints.weightx = 0.5;
+        constraints.weighty   = 0.3;
+        constraints.gridheight = 1;
+        constraints.gridy   = 0  ;
+        constraints.gridx = 0;
+        contentPanel.add(graphField, constraints);
+
+        LeftControlPanel leftPanel = new LeftControlPanel();
+
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        constraints.weighty   = 0.3;
+        constraints.gridx     = 0;
+        constraints.gridy     = 1;
+        sortedGraphField = new SortedGraphField(graph);
+        contentPanel.add(sortedGraphField, constraints);
+
+        constraints.weighty   = 0.0;
+        constraints.gridx     = 0;
+        constraints.gridy     = 2;
+        contentPanel.add(leftPanel, constraints);
+
+        RigthControlPanel cp  = new RigthControlPanel(this.graph, contentPanel, graphField, sortedGraphField);
+        constraints.gridx = 1;      // первая ячейка таблицы по горизонтали
+        constraints.gridy = 0;
+        constraints.gridheight = 3;
+        contentPanel.add(cp, constraints);
+
+        this.add(contentPanel);
+    }
+
+
+}
