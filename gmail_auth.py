2032032a6812c39a5163cff18ad3f4d758419ff4
Дмитрий Левкович <31544742+DmLvkvch@users.noreply.github.com>
2021-02-25 23:51:21 +0300

    Add files via upload

---
+++ gmail_auth.py
@@ -0,0 +1,60 @@
+from __future__ import print_function
+
+import base64
+import email
+import pickle
+import os.path
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from googleapiclient.discovery import build
+
+SCOPES = ['https://www.googleapis.com/auth/gmail.modify']
+
+
+def auth():
+    creds = None
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+    service = build('gmail', 'v1', credentials=creds)
+    return service
+
+
+def get_message(service, user_id, msg_id):
+    try:
+        return service.users().messages().get(userId=user_id, id=msg_id, format='metadata').execute()
+    except Exception as error:
+        print('An error occurred: %s' % error)
+
+
+def get_mime_message(service, user_id, msg_id):
+    try:
+        message = service.users().messages().get(userId=user_id, id=msg_id,
+                                                 format='raw').execute()
+        msg_str = base64.urlsafe_b64decode(message['raw'].encode("utf-8")).decode("utf-8")
+        mime_msg = email.message_from_string(msg_str)
+        return mime_msg
+    except Exception as error:
+        print('An error occurred: %s' % error)
+
+
+def get_mails(service):
+    results = service.users().messages().list(userId='me').execute()
+    return results
+
+
+def change_mail_label(id):
+    pass
+
+
+service = auth()
+get_mails(service)
